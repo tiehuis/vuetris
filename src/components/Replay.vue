df7b903c4c0e7c8be4576036ec98df1def51dcab
Marc Tiehuis <marctiehuis@gmail.com>
2017-12-05 21:48:58 +1300

    Add pagination for replay list

--- src/components/Replay.vue
+++ src/components/Replay.vue
@@ -1,25 +1,39 @@
 <template lang="pug">
   div
     button(v-on:click='hidden = !hidden') Replays
-    ul#replay-list(v-bind:class="{ 'is-hidden': hidden }")
-      li(v-for='replay in this.readOverviews()')
-        // Link a click event to starting a new replay in the main window
-        // Needs to occur at the upper level.
-        span {{ replay.name }} @ {{ new Date(replay.date).toLocaleString() }}: {{ replay.statistics.timeElapsed.toFixed(3) }} - {{ replay.statistics.goal }}
+    div(v-bind:class="{ 'is-hidden': hidden }")
+      ul#replay-list
+        li(v-for='replay in replays.slice(start, end)')
+          // Link a click event to starting a new replay in the main window
+          // Needs to occur at the upper level.
+          span {{ replay.name }} @ {{ new Date(replay.date).toLocaleString() }}: {{ replay.statistics.timeElapsed.toFixed(3) }} - {{ replay.statistics.goal }}
+      label {{ start }} - {{ Math.min(end, replays.length) }} of {{ replays.length }}
+
+      button(v-on:click='prevPage()', :disabled='start == 0') Back
+      button(v-on:click='nextPage()', :disabled='end >= replays.length') Next
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Configuration } from "../tetris/game";
 
+const pageSize = 10;
+
 export default Vue.extend({
   data() {
     return {
-      hidden: true
+      hidden: true,
+      start: 0,
+      end: pageSize
     };
   },
+  computed: {
+    replays: function(): any[] {
+      return this.readOverviews();
+    }
+  },
   methods: {
-    readOverviews: function() {
+    readOverviews() {
       let replays = [];
       for (let i = 0; i < localStorage.length; ++i) {
         const key = localStorage.key(i);
@@ -36,6 +50,19 @@ export default Vue.extend({
 
       replays.sort((a, b) => (a.date < b.date ? 1 : -1));
       return replays;
+    },
+    nextPage() {
+      this.start += pageSize;
+      this.end += pageSize;
+    },
+    prevPage() {
+      this.start -= pageSize;
+      this.end -= pageSize;
+
+      if (this.start < 0) {
+        this.start = 0;
+        this.end = pageSize;
+      }
     }
   }
 });
